9cd23889d417eae43d3732607f2c30cef86d47a0
Damian Galben <damidamianos@gmail.com>
2023-02-13 17:44:20 +0200

    Added some templates, bootstrap and custom css

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
+posts = [
+    {
+        'author': 'Damian Galben',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 12, 2023'
+    },
+    {
+        'author': 'Steve Platon',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 13, 2023'
+    }
+]
 
 # Configure application
 app = Flask(__name__)
@@ -6,9 +21,9 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello, World!</h1>"
+    return render_template('index.html', posts = posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About page!</h1>"
+    return render_template('about.html', title = 'About')
