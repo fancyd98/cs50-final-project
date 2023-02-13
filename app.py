a5532ebce6c6c00439f0423f32884c5f30efd111
Damian Galben <damidamianos@gmail.com>
2023-02-13 22:58:39 +0200

    Add login/register forms and validate user input

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 posts = [
     {
@@ -18,6 +19,9 @@ posts = [
 # Configure application
 app = Flask(__name__)
 
+# Use a secret token
+app.config['SECRET_KEY'] = '6400945f04791b91638e31e1eb31491a'
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -27,3 +31,24 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About')
+
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'Register', form = form)
+
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please Check you username and password', 'danger')
+    return render_template('login.html', title = 'Login', form = form)
