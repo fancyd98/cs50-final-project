2d3599ee1a75e3e80bd9df7680272215bf72e1ae
Damian Galben <damidamianos@gmail.com>
2023-02-14 18:04:27 +0200

    Started setting up SQLAlchemy

--- app.py
+++ app.py
@@ -1,5 +1,41 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
+# Configure application
+app = Flask(__name__)
+
+# Use a secret token
+app.config['SECRET_KEY'] = '6400945f04791b91638e31e1eb31491a'
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///side.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
@@ -15,12 +51,6 @@ posts = [
         'date_posted': 'February 13, 2023'
     }
 ]
-
-# Configure application
-app = Flask(__name__)
-
-# Use a secret token
-app.config['SECRET_KEY'] = '6400945f04791b91638e31e1eb31491a'
 
 @app.route("/")
 @app.route("/home")
@@ -52,3 +82,7 @@ def login():
         else:
             flash('Login Unsuccessful. Please Check you username and password', 'danger')
     return render_template('login.html', title = 'Login', form = form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
