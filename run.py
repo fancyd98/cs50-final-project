dd6b0b079f25dbb662d52c77af04ca5869713e57
Damian Galben <damidamianos@gmail.com>
2023-02-20 22:44:32 +0200

    Added some information

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+'''
+CS50 Final Project Blog
+Name: Damian Galben
+From: Miami, FL, USA
+LinkedIn: https://www.linkedin.com/in/damian-g-037380a6/
+'''
 from blog import create_app
 
 app = create_app()
